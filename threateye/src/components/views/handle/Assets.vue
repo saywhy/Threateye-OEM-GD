9fcd5600ff14462f743c9e2df33a051d4db8d127
yuechanglin <yuechanglin@163.com>
2020-03-15 21:30:53 +0800

    111

--- threateye/src/components/views/handle/Assets.vue
+++ threateye/src/components/views/handle/Assets.vue
@@ -200,10 +200,14 @@
               </template>
             </el-table-column>
             <el-table-column label="威胁等级">
+             <!-- <template slot-scope="scope">
+                <span :class="{'high':scope.row.degree =='high','mid':scope.row.degree =='medium','low':scope.row.degree =='low'}">
+                  {{ scope.row.degree | degree }}</span>
+              </template>-->
               <template slot-scope="scope">
                 <span class="btn_alert_background"
-                      :class="{'high_background':scope.row.degree =='高','mid_background':scope.row.degree =='中','low_background':scope.row.degree =='低'}">
-                  {{ scope.row.degree | degree_sino }}</span>
+                      :class="{'high_background':scope.row.degree =='high','mid_background':scope.row.degree =='medium','low_background':scope.row.degree =='low'}">
+                  {{ scope.row.degree | degree }}</span>
               </template>
             </el-table-column>
             <el-table-column label="失陷确定性">
@@ -435,9 +439,8 @@
                 </el-table-column>
                 <el-table-column label="威胁等级">
                   <template slot-scope="scope">
-                <span class="btn_alert_background"
-                      :class="{'high_background':scope.row.degree =='高','mid_background':scope.row.degree =='中','low_background':scope.row.degree =='低'}">
-                  {{ scope.row.degree | degree_sino }}</span>
+                    <span :class="{'high':scope.row.degree =='high','mid':scope.row.degree =='medium','low':scope.row.degree =='low'}">
+                      {{ scope.row.degree | degree }}</span>
                   </template>
                 </el-table-column>
                 <el-table-column label="失陷确定性">

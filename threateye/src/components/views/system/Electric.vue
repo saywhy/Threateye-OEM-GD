ee7ca2078aa661c97f2ae45fb89226d0a0782b09
zhongshuaisun <zhongshuaisun@126.com>
2020-03-20 21:10:46 +0800

    修改

--- threateye/src/components/views/system/Electric.vue
+++ threateye/src/components/views/system/Electric.vue
@@ -9,17 +9,17 @@
           <el-tab-pane label="网卡配置"
                        class="tabs-item"
                        name="first">
-            <network-card></network-card>
+            <network-card v-if="tab_show.first"></network-card>
           </el-tab-pane>
           <el-tab-pane label="代理服务器"
                        class="tabs-item"
                        name="second">
-            <proxy-server></proxy-server>
+            <proxy-server v-if="tab_show.second"></proxy-server>
           </el-tab-pane>
           <el-tab-pane label="路由设置"
                        class="tabs-item"
                        name="third">
-            <router-set></router-set>
+            <router-set v-if="tab_show.third"></router-set>
           </el-tab-pane>
         </el-tabs>
       </div>
@@ -39,15 +39,40 @@ export default {
   name: "system_control_electric",
   data () {
     return {
-      activeName: "first"
+      activeName: "first",
+      tab_show: {
+        first: true,
+        second: false,
+        third: false,
+      }
     };
   },
   mounted () {
   },
   methods: {
     handleClick (tab, event) {
-      console.log(tab.label);
-    },
+      console.log(tab);
+      switch (tab.name) {
+        case "first":
+          this.tab_show.first = true;
+          this.tab_show.second = false;
+          this.tab_show.third = false;
+          break;
+        case "second":
+          this.tab_show.first = false;
+          this.tab_show.second = true;
+          this.tab_show.third = false;
+          break;
+        case "third":
+          this.tab_show.first = false;
+          this.tab_show.second = false;
+          this.tab_show.third = true;
+          break;
+        default:
+          break;
+      }
+
+    }
 
   }
 };
